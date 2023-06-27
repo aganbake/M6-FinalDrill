a61d9e62f3e0f920b902e2002034fbf193a5a67d
aganbake <abdul.villarroel@gmail.com>
2023-06-27 19:03:25 -0400

    Initial Release

---
+++ index.js
@@ -0,0 +1,13 @@
+// Modules
+const express = require("express");
+
+// Requires
+const app = express();
+const PORT = 3000;
+
+// CRUD Operations
+
+// Routes
+
+// Server
+app.listen(PORT, () => `Server Up and running on port ${PORT}`);
